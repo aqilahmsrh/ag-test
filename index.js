4d1ace5bccf9956b2fdfbbb80d8bed66453ae73f
aqilahmsrh <127908263+aqilahmsrh@users.noreply.github.com>
2023-03-30 15:51:56 +0800

    m

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send("welcome to express app!!");
+  res.send("Hello ReGov");
 });
 
 app.get("/info", (req, res) => {
